6d9cb4f72a9c59df8ec9d6df00dc5a5b068eef60
baisui <baisui@2dfire.com>
2019-10-06 20:23:31 +0800

    fix GroupByAndCountSearchComponent remove debug code

--- tis-index-builder-yarn/src/test/java/Test.java
+++ tis-index-builder-yarn/src/test/java/Test.java
@@ -1,3 +1,4 @@
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Date;
 
@@ -9,6 +10,10 @@ import java.util.Date;
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println(URLEncoder.encode("pt=2019092714(\\d{4})"));
+		//System.out.println(URLEncoder.encode("pt=2019092714(\\d{4})"));
+		
+	 System.out.println(	URLEncoder.encode(	"tis%E4%BB%8B%E7%BB%8D%E4%B8%8E%E6%95%B4%E4%BD%93%E6%9E%B6%E6%9E%84") );
+	 
+	 System.out.println(	URLDecoder.decode( "#tis%25E4%25BB%258B%25E7%25BB%258D%25E4%25B8%258E%25E6%2595%25B4%25E4%25BD%2593%25E6%259E%25B6%25E6%259E%2584"));
 	}
 }
--- tis-solrcore-extend/src/main/java/com/qlangtech/tis/solrextend/handler/component/GroupByAndCountSearchComponent.java
+++ tis-solrcore-extend/src/main/java/com/qlangtech/tis/solrextend/handler/component/GroupByAndCountSearchComponent.java
@@ -1,24 +1,17 @@
 package com.qlangtech.tis.solrextend.handler.component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.index.NumericDocValues;
 import org.apache.lucene.index.SortedDocValues;
 import org.apache.lucene.search.SimpleCollector;
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.cloud.CloudDescriptor;
-import org.apache.solr.cloud.ZkController;
-import org.apache.solr.common.SolrException;
-//import org.apache.solr.common.StringUtils;
-import org.apache.solr.common.cloud.Replica;
-import org.apache.solr.common.cloud.Slice;
-import org.apache.solr.common.params.CommonParams;
+import org.apache.lucene.util.PriorityQueue;
 import org.apache.solr.common.params.ShardParams;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.SimpleOrderedMap;
@@ -27,6 +20,8 @@ import org.apache.solr.handler.component.SearchComponent;
 import org.apache.solr.handler.component.ShardRequest;
 import org.apache.solr.handler.component.ShardResponse;
 import org.apache.solr.search.DocListAndSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
 
@@ -42,16 +37,24 @@ public class GroupByAndCountSearchComponent extends SearchComponent {
 
 	private static final String NAME = "group_by_count";
 	private static final String TIS_GROUP_FIELD = "tis.group.field";
+	private static final String TIS_GROUP_LIMIT = "tis.group.limit";
 
-	private String groupBy;
-	private DVType dvType;
+	private static final Logger logger = LoggerFactory.getLogger(GroupByAndCountSearchComponent.class);
 
 	@Override
 	public void prepare(ResponseBuilder rb) throws IOException {
-		if (rb.req.getParams().getBool(NAME, false)) {
+
+		SolrParams params = rb.req.getParams();
+		Integer purpose = params.getInt(ShardParams.SHARDS_PURPOSE);
+		if (purpose != null && (purpose & ShardRequest.PURPOSE_GET_FIELDS) != 0) {
+			return;
+		}
+
+		if (params.getBool(NAME, false)) {
 			rb.setNeedDocSet(true);
-			SolrParams params = rb.req.getParams();
-			String groupField = params.get(TIS_GROUP_FIELD);
+			SolrParams r = rb.req.getParams();
+			int groupLimit = r.getInt(TIS_GROUP_LIMIT, 50);
+			String groupField = r.get(TIS_GROUP_FIELD);
 			if (StringUtils.isEmpty(groupField)) {
 				throw new IllegalArgumentException("param:" + TIS_GROUP_FIELD + " can not be empty");
 			}
@@ -59,54 +62,61 @@ public class GroupByAndCountSearchComponent extends SearchComponent {
 			if (groupFieldArg.length != 2) {
 				throw new IllegalArgumentException("param:" + TIS_GROUP_FIELD + " is not illegal");
 			}
-			this.groupBy = groupFieldArg[0];
-			dvType = DVType.parse(groupFieldArg[1]);
+			rb.req.getContext().put(NAME,
+					new GroupByCountContext(groupFieldArg[0], DVType.parse(groupFieldArg[1]), groupLimit));
+
+			log("prep");
 		}
+	}
+
+	private void log(String message) {
+		// logger.warn("{}ip:{}->{}", Thread.currentThread().hashCode(),
+		// MDC.get("ip"), message);
 	}
 
 	@Override
 	public void process(ResponseBuilder rb) throws IOException {
-		if (!rb.req.getParams().getBool(NAME, false)) {
+		GroupByCountContext countContext = null;
+		if ((countContext = (GroupByCountContext) rb.req.getContext().get(NAME)) == null) {
 			return;
 		}
 
 		DocListAndSet results = rb.getResults();
-
-		CountCollector collector = new CountCollector(groupBy, this.dvType);
+		CountCollector collector = new CountCollector(countContext);
 		rb.req.getSearcher().search(results.docSet.getTopFilter(), collector);
 
+		//log("process," + collector.segmentSummary.toString());
 		rb.rsp.add(NAME, ResultUtils.writeMap(collector.getGroupByCount()));
-
 	}
 
-	@Override
-	public int distributedProcess(ResponseBuilder rb) throws IOException {
-		if (!rb.req.getParams().getBool(NAME, false)) {
-			return ResponseBuilder.STAGE_DONE;
-		}
-
-		if (rb.stage == ResponseBuilder.STAGE_GET_FIELDS) {
-			createSubRequests(rb);
-		}
-		return ResponseBuilder.STAGE_DONE;
-	}
+	// @Override
+	// public int distributedProcess(ResponseBuilder rb) throws IOException {
+	// if (!rb.req.getParams().getBool(NAME, false)) {
+	// return ResponseBuilder.STAGE_DONE;
+	// }
+	//
+	// if (rb.stage == ResponseBuilder.STAGE_GET_FIELDS) {
+	// createSubRequests(rb);
+	// }
+	// return ResponseBuilder.STAGE_DONE;
+	// }
 
 	@Override
 	@SuppressWarnings("all")
 	public void handleResponses(ResponseBuilder rb, ShardRequest sreq) {
-		if (!rb.req.getParams().getBool(NAME, false)) {
+		GroupByCountContext countContext = null;
+		if ((countContext = (GroupByCountContext) rb.req.getContext().get(NAME)) == null) {
 			return;
 		}
 		if (rb.stage != ResponseBuilder.STAGE_EXECUTE_QUERY) {
 			return;
 		}
-		// super.handleResponses(rb, sreq);
-		// logger.info("handleResponses,response size:" +
-		// sreq.responses.size());
+
 		SimpleOrderedMap<String> shardResult = null;
-		Map<String, AtomicInteger> mergeResult = new HashMap<>();
+		Map<String /* group key */, AtomicInteger> mergeResult = new HashMap<>();
 		AtomicInteger val = null;
 		String key = null;
+		// 将几个分片数据合并
 		for (ShardResponse sr : sreq.responses) {
 			shardResult = (SimpleOrderedMap<String>) sr.getSolrResponse().getResponse().get(NAME);
 			for (int i = 0; i < shardResult.size(); i++) {
@@ -118,73 +128,54 @@ public class GroupByAndCountSearchComponent extends SearchComponent {
 				val.addAndGet(Integer.parseInt(shardResult.getVal(i)));
 			}
 		}
+		DocVal top = null;
+		int hitCount;
+		SortQueue sortQueue = new SortQueue(countContext.groupLimit);
+		for (Map.Entry<String /* group key */, AtomicInteger> entry : mergeResult.entrySet()) {
+			if ((hitCount = entry.getValue().get()) > (top = sortQueue.top()).count) {
+				top.count = hitCount;
+				top.keyVal = entry.getKey();
+				sortQueue.updateTop();
+			}
+		}
+
+		mergeResult.clear();
+		while ((top = sortQueue.pop()) != null) {
+			mergeResult.put(top.keyVal, new AtomicInteger(top.count));
+		}
+
+		log("handleResponses,mergeResult.size:" + mergeResult.size());
 
 		rb.rsp.add(NAME, ResultUtils.writeMap(mergeResult));
 	}
 
-	private void createSubRequests(ResponseBuilder rb) throws IOException {
-		SolrParams params = rb.req.getParams();
-		CloudDescriptor cloudDescriptor = rb.req.getCore().getCoreDescriptor().getCloudDescriptor();
-		ZkController zkController = rb.req.getCore().getCoreContainer().getZkController();
-		String collection = cloudDescriptor.getCollectionName();
-
-		for (Slice slice : zkController.getClusterState().getCollection(cloudDescriptor.getCollectionName())
-				.getActiveSlices()) {
-			String shard = slice.getName();
-			ShardRequest sreq = new ShardRequest();
-
-			sreq.purpose = 1;
-			sreq.shards = sliceToShards(rb, collection, slice);
-			sreq.actualShards = sreq.shards;
-
-			SolrQuery squery = new SolrQuery();
-			squery.set(ShardParams.SHARDS_QT, "/select");
-			squery.set(TIS_GROUP_FIELD, this.groupBy+":"+dvType.type);
-			squery.set(NAME, true);
-			squery.setStart(0);
-			squery.setRows(0);
-			// String fields = params.get(CommonParams.FL);
-			// if (fields != null) {
-			// squery.set(CommonParams.FL, fields);
-			// }
-			// squery.set(CommonParams.DISTRIB, false);
-			squery.setQuery(params.get(CommonParams.Q));
-			sreq.params = squery;
-			// sreq.params.set(CommonParams.DISTRIB, false);
-
-			rb.addRequest(this, sreq);
-		}
-	}
-
-	private String[] sliceToShards(ResponseBuilder rb, String collection, Slice slice) {
-
-		Optional<Replica> replics = slice.getReplicas().stream().findFirst();
-		if (!replics.isPresent()) {
-			throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,
-					"Can't find shard '" + slice.getName() + "',all slice[" + rb.slices.length + "]");
-		}
-
-		return new String[] { replics.get().getCoreUrl() };
-	}
 
 	private static class CountCollector extends SimpleCollector {
 		private final String groupByField;
 		// private SortedDocValues groupFieldDV = null;
 
 		private DVProcess dvProcessor;
-		private Map<Order/* order */, AtomicInteger> groupByCountSegment;
-
+		// private Map<Order/* order */, AtomicInteger> groupByCountSegment;
+		private int[] groupByCountSegment;
 		private final Map<String/* groupField */, AtomicInteger> groupByCount = Maps.newHashMap();
 		private final DVType dvType;
-		private final Order offset = new Order();
 
-		public CountCollector(String groupByField, DVType dvType) {
+		private final int sortQueueLimit;
+		// private final Order offset = new Order();
+		private SortQueue sortQueue;
+
+		//private int segmentCount;
+		//private StringBuffer segmentSummary = new StringBuffer();
+
+		public CountCollector(GroupByCountContext countContext) {
 			super();
-			this.groupByField = groupByField;
-			if (dvType == null) {
-				throw new IllegalArgumentException("param dvType can not be null");
+			if (countContext == null) {
+				throw new IllegalArgumentException("countContext can not be null");
 			}
-			this.dvType = dvType;
+			this.groupByField = countContext.groupBy;
+			this.dvType = countContext.dvType;
+			this.sortQueueLimit = countContext.groupLimit;
+
 		}
 
 		public Map<String, AtomicInteger> getGroupByCount() {
@@ -202,66 +193,54 @@ public class GroupByAndCountSearchComponent extends SearchComponent {
 
 		protected void doSetNextReader(LeafReaderContext context) throws IOException {
 			mergeResult();
+			this.groupByCountSegment = new int[context.reader().maxDoc()];
 			this.dvProcessor = dvType.createDVProcess(context, groupByField);
-			this.groupByCountSegment = Maps.newHashMap();
+			this.sortQueue = new SortQueue(this.sortQueueLimit);
+//			segmentSummary.append("\nsegment_").append(++segmentCount).append(",length:")
+//					.append(this.groupByCountSegment.length);
+
 		}
 
 		private void mergeResult() throws IOException {
 
 			if (dvProcessor != null) {
-				String fieldVal = null;
 				AtomicInteger groupCount = null;
-				for (Map.Entry<Order, AtomicInteger> entry : groupByCountSegment.entrySet()) {
-
-					fieldVal = this.dvProcessor.getValByOrder(entry.getKey().val);
-
-					groupCount = groupByCount.get(fieldVal);
+				int hitCount;
+				DocVal top = null;
+				//int[] count = new int[1];
+				for (int i = 0; i < this.groupByCountSegment.length; i++) {
+					if ((hitCount = groupByCountSegment[i]) > 0) {
+						if (hitCount > (top = sortQueue.top()).count) {
+							top.count = hitCount;
+							top.keyVal = this.dvProcessor.getValByOrder(i);
+							sortQueue.updateTop();
+						}
+					}
+				}
+				//segmentSummary.append(",hit:").append(count[0]);
+				while ((top = this.sortQueue.pop()) != null) {
+					groupCount = groupByCount.get(top.keyVal);
 					if (groupCount == null) {
 						groupCount = new AtomicInteger();
-						groupByCount.put(fieldVal, groupCount);
+						groupByCount.put(top.keyVal, groupCount);
 					}
-					groupCount.addAndGet(entry.getValue().get());
+					groupCount.addAndGet(top.count);
 				}
-				this.dvProcessor = null;
 			}
+
+			this.dvProcessor = null;
 		}
 
 		@Override
 		public void collect(int doc) throws IOException {
-
-			Order order = offset.setVal(dvProcessor.ordValue(doc));
-			AtomicInteger count = groupByCountSegment.get(order);
-			if (count == null) {
-				count = new AtomicInteger();
-				groupByCountSegment.put(order.copy(), count);
+			int ord = dvProcessor.ordValue(doc);
+			try {
+				groupByCountSegment[ord]++;
+			} catch (Exception e) {
+				throw new RuntimeException("ord:" + ord, e);
 			}
-			// 自增一下
-			count.incrementAndGet();
-
-		}
-	}
-
-	private static class Order {
-		private long val;
-
-		private Order setVal(long order) {
-			this.val = order;
-			return this;
 		}
 
-		public Order copy() {
-			return (new Order()).setVal(this.val);
-		}
-
-		@Override
-		public int hashCode() {
-			return Long.hashCode(val);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			return obj.hashCode() == this.val;
-		}
 	}
 
 	@Override
@@ -271,34 +250,38 @@ public class GroupByAndCountSearchComponent extends SearchComponent {
 
 	public enum DVType {
 		Numeric("numeric", (context, fname) -> {
-			final NumericDocValues numericGroupDV = org.apache.lucene.index.DocValues.getNumeric(context.reader(),
-					fname);
-
-			return new DVProcess() {
-				public long ordValue(int docId) throws IOException {
-					numericGroupDV.advance(docId);
-					return numericGroupDV.longValue();
-				}
-
-				public String getValByOrder(long ord) throws IOException {
-					return String.valueOf(ord);
-				}
-			};
-		}), Str("string", (context, fname) -> {
+			// final NumericDocValues numericGroupDV =
+			// org.apache.lucene.index.DocValues.getNumeric(context.reader(),
+			// fname);
+			throw new UnsupportedOperationException();
+			// return new DVProcess() {
+			// public int ordValue(int docId) throws IOException {
+			// // numericGroupDV.advance(docId);
+			// // return numericGroupDV.longValue();
+			// throw new UnsupportedOperationException();
+			// }
+			//
+			// public String getValByOrder(int ord) throws IOException {
+			// throw new UnsupportedOperationException();
+			// }
+			// };
+		})//
+		, Str("string", (context, fname) -> {
 
 			final SortedDocValues groupFieldDV = org.apache.lucene.index.DocValues.getSorted(context.reader(), fname);
 
 			return new DVProcess() {
-				public long ordValue(int docId) throws IOException {
+				public int ordValue(int docId) throws IOException {
 					groupFieldDV.advance(docId);
 					return groupFieldDV.ordValue();
 				}
 
-				public String getValByOrder(long ord) throws IOException {
-					return groupFieldDV.lookupOrd((int) ord).utf8ToString();
+				public String getValByOrder(int ord) throws IOException {
+					return groupFieldDV.lookupOrd(ord).utf8ToString();
 				}
 			};
 		});
+
 		private final String type;
 		private final DVProcessCreator dvProcessCreator;
 
@@ -312,24 +295,86 @@ public class GroupByAndCountSearchComponent extends SearchComponent {
 		}
 
 		public static DVType parse(String type) {
-			if (Numeric.type.equals(type)) {
-				return Numeric;
-			} else if (Str.type.equals(type)) {
+			if (Str.type.equals(type)) {
 				return Str;
 			}
-
 			throw new IllegalStateException("type:" + type + " is illegal");
 		}
 	}
 
 	private interface DVProcess {
-		long ordValue(int docId) throws IOException;
+		int ordValue(int docId) throws IOException;
 
-		String getValByOrder(long ord) throws IOException;
+		String getValByOrder(int ord) throws IOException;
 	}
 
 	private interface DVProcessCreator {
 		public DVProcess createDVProcess(LeafReaderContext context, String fieldName) throws IOException;
+	}
+
+	static class SortQueue extends PriorityQueue<DocVal> {
+		public SortQueue(int len) {
+			super(len, () -> new DocVal(Integer.MIN_VALUE));
+		}
+
+		protected boolean lessThan(DocVal t1, DocVal t2) {
+			return t1.count < t2.count;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		SortQueue queue = new SortQueue(10);
+		List<DocVal> docs = new ArrayList<>();
+		DocVal doc = null;
+		for (int i = 0; i < 100; i++) {
+			doc = new DocVal((int) (Math.random() * 300));
+			docs.add(doc);
+			System.out.println(doc.count);
+		}
+
+		DocVal top = null;
+
+		System.out.println("============================================");
+		for (DocVal d : docs) {
+			if (d.count > (top = queue.top()).count) {
+				top.count = d.count;
+				top.docBase = d.docBase;
+				top.keyVal = "";
+				queue.updateTop();
+			}
+		}
+		System.out.println("============================================");
+		while ((top = queue.pop()) != null) {
+			System.out.println("count:" + top.count + ",docId:" + top.keyVal);
+		}
+
+	}
+
+	private static class DocVal {
+		private String keyVal;
+		private int docBase;
+
+		public DocVal(int count) {
+			super();
+			this.count = count;
+		}
+
+		public int count;
+	}
+
+	private static class GroupByCountContext {
+		private final String groupBy;
+		private final DVType dvType;
+		private final int groupLimit;
+
+		public GroupByCountContext(String groupBy, DVType dvType, int groupLimit) {
+			super();
+			this.groupBy = groupBy;
+			this.dvType = dvType;
+			this.groupLimit = groupLimit;
+		}
+
 	}
 
 }
