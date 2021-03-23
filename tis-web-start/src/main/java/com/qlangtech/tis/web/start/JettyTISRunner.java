11c58c53122ac65f249fd89d799c10bfcc4f5297
莫正华 <baisui@qlangtech.com>
2021-03-23 12:27:26 +0800

    update

--- tis-web-start/src/main/java/com/qlangtech/tis/web/start/JettyTISRunner.java
+++ tis-web-start/src/main/java/com/qlangtech/tis/web/start/JettyTISRunner.java
@@ -137,7 +137,7 @@ public class JettyTISRunner {
             // contextCloassLoader.addClassPath(Resource.newResource(confDir));
             jarfiles.add(confDir.toURI().toURL());
             TISAppClassLoader contextCloassLoader
-                    = new TISAppClassLoader(context, Thread.currentThread().getContextClassLoader(), jarfiles.toArray(new URL[jarfiles.size()]));
+                    = new TISAppClassLoader(context, JettyTISRunner.class.getClassLoader(), jarfiles.toArray(new URL[jarfiles.size()]));
             webAppContext.setClassLoader(contextCloassLoader);
         } else {
             webAppContext.setClassLoader(this.getClass().getClassLoader());
