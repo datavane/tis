3bf075c8fe368be90142ba6aa06acf5bbff0bdc3
莫正华 <baisui@qlangtech.com>
2021-03-23 11:29:20 +0800

    add jndi datasource register support for share derby in collectinfo and tis-console module

--- tis-console/src/main/java/com/qlangtech/tis/runtime/module/action/SysInitializeArrangeJars.java
+++ tis-console/src/main/java/com/qlangtech/tis/runtime/module/action/SysInitializeArrangeJars.java
@@ -21,7 +21,6 @@ import com.qlangtech.tis.util.Memoizer;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -43,12 +42,22 @@ public class SysInitializeArrangeJars {
   private static final String tis_manage_pojo = "tis-manage-pojo(.*)\\.jar";
   private static final String spring_web = "spring-web-(.*)\\.jar";
 
+  private static final String commons_ = "commons-(.*)\\.jar";
+
+//  private static final List<SubProj> subDirs
+//    = Lists.newArrayList( //
+//    new SubProj("tis-assemble", "tis-assemble\\.jar", tis_builder_api, tis_manage_pojo, spring_web) //
+//    , new SubProj("solr", "solr\\.jar", tis_builder_api, tis_manage_pojo, spring_web)
+//    , new SubProj("tjs", "tis\\.jar", tis_builder_api, tis_manage_pojo, spring_web)
+//    , new SubProj("tis-collect", "tis-collect\\.jar", tis_builder_api, tis_manage_pojo, spring_web));
+
+
   private static final List<SubProj> subDirs
     = Lists.newArrayList( //
-    new SubProj("tis-assemble", "tis-assemble\\.jar", tis_builder_api, tis_manage_pojo, spring_web) //
-    , new SubProj("solr", "solr\\.jar", tis_builder_api, tis_manage_pojo, spring_web)
-    , new SubProj("tjs", "tis\\.jar", tis_builder_api, tis_manage_pojo, spring_web)
-    , new SubProj("tis-collect", "tis-collect\\.jar", tis_builder_api, tis_manage_pojo, spring_web));
+    new SubProj("tis-assemble", commons_) //
+    , new SubProj("solr", commons_)
+    , new SubProj("tjs", commons_)
+    , new SubProj("tis-collect", commons_));
 
   static final Memoizer<String, List<File>> jars = new Memoizer<String, List<File>>() {
     @Override
@@ -66,51 +75,62 @@ public class SysInitializeArrangeJars {
       throw new IllegalStateException("uberDir is not exist:" + uberDir.getAbsolutePath());
     }
     File subModuleLibDir = null;
-    for (SubProj sbDir : subDirs) {
-      subModuleLibDir = new File(uberDir, sbDir.getName() + "/lib");
-      if (!subModuleLibDir.exists()) {
-        throw new IllegalStateException("sub lib dir:" + subModuleLibDir.getAbsolutePath() + " is not exist");
-      }
-      for (String jarFileName : subModuleLibDir.list((dir, name) -> !sbDir.shallRetain(name))) {
-        jars.get(jarFileName).add(new File(subModuleLibDir, jarFileName));
-      }
-    }
+
     File webStartDir = new File(uberDir, "web-start/lib");
     Set<String> existJarFiles = Sets.newHashSet(webStartDir.list());
     if (existJarFiles.size() < 1) {
       throw new IllegalStateException("webStartDir:" + webStartDir.getAbsolutePath() + " has any jar file");
     }
 
+    // 将web-start中已有jar在子工程中去掉
+    for (SubProj sbDir : subDirs) {
+      subModuleLibDir = new File(uberDir, sbDir.getName() + "/lib");
+      if (!subModuleLibDir.exists()) {
+        throw new IllegalStateException("sub lib dir:" + subModuleLibDir.getAbsolutePath() + " is not exist");
+      }
+      for (String jarFileName : subModuleLibDir.list()) {
+        if (existJarFiles.contains(jarFileName)) {
+          FileUtils.deleteQuietly(new File(subModuleLibDir, jarFileName));
+        }
+      }
+    }
+
+    for (SubProj sbDir : subDirs) {
+      subModuleLibDir = new File(uberDir, sbDir.getName() + "/lib");
+      if (!subModuleLibDir.exists()) {
+        throw new IllegalStateException("sub lib dir:" + subModuleLibDir.getAbsolutePath() + " is not exist");
+      }
+      for (String jarFileName : subModuleLibDir.list()) {
+        if (sbDir.isMatch(jarFileName)) {
+          jars.compute(jarFileName).add(new File(subModuleLibDir, jarFileName));
+        }
+      }
+    }
+
+
+    boolean first = true;
     for (Map.Entry<String, List<File>> subModuleJar : jars.getEntries()) {
       System.out.println("process file:" + subModuleJar.getKey());
-      if (existJarFiles.contains(subModuleJar.getKey())) {
-        subModuleJar.getValue().forEach((f) -> {
-          System.out.println("delete:" + f.getAbsolutePath());
-          forceDeleteOnExit(f);
-        });
-      } else {
-        boolean first = true;
-        for (File subJar : subModuleJar.getValue()) {
-          if (first) {
-            FileUtils.moveFile(subJar, new File(webStartDir, subModuleJar.getKey()));
-            first = false;
-          } else {
-            forceDeleteOnExit(subJar);
-            System.out.println("delete:" + subJar.getAbsolutePath());
-          }
+
+      for (File f : subModuleJar.getValue()) {
+        if (first) {
+          FileUtils.moveFile(f, new File(webStartDir, subModuleJar.getKey()));
+          first = false;
+        } else {
+          FileUtils.deleteQuietly(f);
         }
       }
     }
   }
 
 
-  private static void forceDeleteOnExit(File f) {
-    try {
-      FileUtils.forceDeleteOnExit(f);
-    } catch (IOException e) {
-      throw new IllegalStateException("path:" + f.getAbsolutePath(), e);
-    }
-  }
+//  private static void forceDeleteOnExit(File f) {
+//    try {
+//      FileUtils.forceDeleteOnExit(f);
+//    } catch (IOException e) {
+//      throw new IllegalStateException("path:" + f.getAbsolutePath(), e);
+//    }
+//  }
 
   private static class SubProj extends Option {
     // 需要保留的jar包
@@ -124,7 +144,7 @@ public class SysInitializeArrangeJars {
       }
     }
 
-    public boolean shallRetain(String jarFileName) {
+    public boolean isMatch(String jarFileName) {
       for (Pattern p : retainJars) {
         if (p.matcher(jarFileName).matches()) {
           return true;
