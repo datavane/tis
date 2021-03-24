163bee7ca7e1eedbafa797cdb13183e164ce7826
莫正华 <baisui@qlangtech.com>
2021-03-24 16:40:51 +0800

    modify now() to CURRENT_TIMESTAMP for derby comparity

--- tis-console/src/main/java/com/qlangtech/tis/manage/biz/dal/dao/impl/ClusterSnapshotDAOImpl.java
+++ tis-console/src/main/java/com/qlangtech/tis/manage/biz/dal/dao/impl/ClusterSnapshotDAOImpl.java
@@ -1,14 +1,14 @@
 /**
  * Copyright (c) 2020 QingLang, Inc. <baisui@qlangtech.com>
- *
+ * <p>
  * This program is free software: you can use, redistribute, and/or modify
  * it under the terms of the GNU Affero General Public License, version 3
  * or later ("AGPL"), as published by the Free Software Foundation.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -19,6 +19,9 @@ import com.qlangtech.tis.manage.biz.dal.pojo.ClusterSnapshot;
 import com.qlangtech.tis.manage.biz.dal.pojo.ClusterSnapshotCriteria;
 import com.qlangtech.tis.manage.biz.dal.pojo.ClusterSnapshotQuery;
 import com.qlangtech.tis.manage.common.BasicDAO;
+import com.qlangtech.tis.manage.common.Config;
+
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -27,87 +30,92 @@ import java.util.List;
  */
 public class ClusterSnapshotDAOImpl extends BasicDAO<ClusterSnapshot, ClusterSnapshotCriteria> implements IClusterSnapshotDAO {
 
-    @Override
-    public String getEntityName() {
-        return "cluster_snapshot_dao";
+  @Override
+  public String getEntityName() {
+    return "cluster_snapshot_dao";
+  }
+
+  public ClusterSnapshotDAOImpl() {
+    super();
+  }
+
+  public int countByExample(ClusterSnapshotCriteria example) {
+    Integer count = this.count("cluster_snapshot.ibatorgenerated_countByExample", example);
+    return count;
+  }
+
+  public int countFromWriteDB(ClusterSnapshotCriteria example) {
+    Integer count = this.countFromWriterDB("cluster_snapshot.ibatorgenerated_countByExample", example);
+    return count;
+  }
+
+  public int deleteByExample(ClusterSnapshotCriteria criteria) {
+    return this.deleteRecords("cluster_snapshot.ibatorgenerated_deleteByExample", criteria);
+  }
+
+  public Long insert(ClusterSnapshot record) {
+    Object newKey = this.insert("cluster_snapshot.ibatorgenerated_insert", record);
+    return (Long) newKey;
+  }
+
+  public Long insertSelective(ClusterSnapshot record) {
+    Object newKey = this.insert("cluster_snapshot.ibatorgenerated_insertSelective", record);
+    return (Long) newKey;
+  }
+
+  /**
+   * @param query
+   * @return
+   */
+  public List<ClusterSnapshot> reportClusterStatus(ClusterSnapshotQuery query) {
+
+    if (Config.DB_TYPE_DERBY.equals(Config.getDbCfg().dbtype)) {
+      return Collections.emptyList();
     }
 
-    public ClusterSnapshotDAOImpl() {
-        super();
+    return this.listAnonymity("cluster_snapshot.clusterReport" + query.getSqlmapSuffix(), query);
+  }
+
+  public List<ClusterSnapshot> selectByExample(ClusterSnapshotCriteria criteria) {
+    return this.selectByExample(criteria, 1, 100);
+  }
+
+  @SuppressWarnings("all")
+  public List<ClusterSnapshot> selectByExample(ClusterSnapshotCriteria example, int page, int pageSize) {
+    example.setPage(page);
+    example.setPageSize(pageSize);
+    List<ClusterSnapshot> list = this.list("cluster_snapshot.ibatorgenerated_selectByExample", example);
+    return list;
+  }
+
+  // public ClusterSnapshot selectByPrimaryKey(Long id) {
+  // ClusterSnapshot key = new ClusterSnapshot();
+  // key.setId(id);
+  // ClusterSnapshot record = (ClusterSnapshot) this.load("cluster_snapshot.ibatorgenerated_selectByPrimaryKey",
+  // key);
+  // return record;
+  // }
+  public int updateByExampleSelective(ClusterSnapshot record, ClusterSnapshotCriteria example) {
+    UpdateByExampleParms parms = new UpdateByExampleParms(record, example);
+    return this.updateRecords("cluster_snapshot.ibatorgenerated_updateByExampleSelective", parms);
+  }
+
+  public int updateByExample(ClusterSnapshot record, ClusterSnapshotCriteria example) {
+    UpdateByExampleParms parms = new UpdateByExampleParms(record, example);
+    return this.updateRecords("cluster_snapshot.ibatorgenerated_updateByExample", parms);
+  }
+
+  private static class UpdateByExampleParms extends ClusterSnapshotCriteria {
+
+    private Object record;
+
+    public UpdateByExampleParms(Object record, ClusterSnapshotCriteria example) {
+      super(example);
+      this.record = record;
     }
 
-    public int countByExample(ClusterSnapshotCriteria example) {
-        Integer count = this.count("cluster_snapshot.ibatorgenerated_countByExample", example);
-        return count;
+    public Object getRecord() {
+      return record;
     }
-
-    public int countFromWriteDB(ClusterSnapshotCriteria example) {
-        Integer count = this.countFromWriterDB("cluster_snapshot.ibatorgenerated_countByExample", example);
-        return count;
-    }
-
-    public int deleteByExample(ClusterSnapshotCriteria criteria) {
-        return this.deleteRecords("cluster_snapshot.ibatorgenerated_deleteByExample", criteria);
-    }
-
-    public Long insert(ClusterSnapshot record) {
-        Object newKey = this.insert("cluster_snapshot.ibatorgenerated_insert", record);
-        return (Long) newKey;
-    }
-
-    public Long insertSelective(ClusterSnapshot record) {
-        Object newKey = this.insert("cluster_snapshot.ibatorgenerated_insertSelective", record);
-        return (Long) newKey;
-    }
-
-    /**
-     * @param query
-     * @return
-     */
-    public List<ClusterSnapshot> reportClusterStatus(ClusterSnapshotQuery query) {
-        return this.listAnonymity("cluster_snapshot.clusterReport" + query.getSqlmapSuffix(), query);
-    }
-
-    public List<ClusterSnapshot> selectByExample(ClusterSnapshotCriteria criteria) {
-        return this.selectByExample(criteria, 1, 100);
-    }
-
-    @SuppressWarnings("all")
-    public List<ClusterSnapshot> selectByExample(ClusterSnapshotCriteria example, int page, int pageSize) {
-        example.setPage(page);
-        example.setPageSize(pageSize);
-        List<ClusterSnapshot> list = this.list("cluster_snapshot.ibatorgenerated_selectByExample", example);
-        return list;
-    }
-
-    // public ClusterSnapshot selectByPrimaryKey(Long id) {
-    // ClusterSnapshot key = new ClusterSnapshot();
-    // key.setId(id);
-    // ClusterSnapshot record = (ClusterSnapshot) this.load("cluster_snapshot.ibatorgenerated_selectByPrimaryKey",
-    // key);
-    // return record;
-    // }
-    public int updateByExampleSelective(ClusterSnapshot record, ClusterSnapshotCriteria example) {
-        UpdateByExampleParms parms = new UpdateByExampleParms(record, example);
-        return this.updateRecords("cluster_snapshot.ibatorgenerated_updateByExampleSelective", parms);
-    }
-
-    public int updateByExample(ClusterSnapshot record, ClusterSnapshotCriteria example) {
-        UpdateByExampleParms parms = new UpdateByExampleParms(record, example);
-        return this.updateRecords("cluster_snapshot.ibatorgenerated_updateByExample", parms);
-    }
-
-    private static class UpdateByExampleParms extends ClusterSnapshotCriteria {
-
-        private Object record;
-
-        public UpdateByExampleParms(Object record, ClusterSnapshotCriteria example) {
-            super(example);
-            this.record = record;
-        }
-
-        public Object getRecord() {
-            return record;
-        }
-    }
+  }
 }
