fbc44b0dcc3d8d908f580204fbd89f7620afe71d
mozhenghua <baisui@qlangtech.com>
2021-11-17 16:09:59 +0800

    remove tis-datax-executor from tis-assemble

---
+++ tis-assemble/src/test/java/com/qlangtech/tis/datax/TestDataXJobSubmit.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2020 QingLang, Inc. <baisui@qlangtech.com>
+ * <p>
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3
+ * or later ("AGPL"), as published by the Free Software Foundation.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.qlangtech.tis.datax;
+
+import junit.framework.TestCase;
+
+import java.util.Optional;
+
+/**
+ * @author: 百岁（baisui@qlangtech.com）
+ * @create: 2021-11-17 15:48
+ **/
+public class TestDataXJobSubmit extends TestCase {
+    public void testGetLocalJobSubmit() {
+        Optional<DataXJobSubmit> dataXJobSubmit = DataXJobSubmit.getDataXJobSubmit(DataXJobSubmit.InstanceType.LOCAL);
+        assertTrue(dataXJobSubmit.isPresent());
+    }
+}
