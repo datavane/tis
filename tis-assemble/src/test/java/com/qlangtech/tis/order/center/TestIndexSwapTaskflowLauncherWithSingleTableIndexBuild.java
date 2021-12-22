f9d1e639a12094f0cca8918fff4c13f83e9f0277
mozhenghua <baisui@qlangtech.com>
2021-12-22 13:57:20 +0800

    remote Solr dependency

--- tis-assemble/src/test/java/com/qlangtech/tis/order/center/TestIndexSwapTaskflowLauncherWithSingleTableIndexBuild.java
+++ tis-assemble/src/test/java/com/qlangtech/tis/order/center/TestIndexSwapTaskflowLauncherWithSingleTableIndexBuild.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qlangtech.tis.order.center;
 
@@ -40,7 +40,6 @@ import com.qlangtech.tis.offline.TableDumpFactory;
 import com.qlangtech.tis.order.dump.task.ITableDumpConstant;
 import com.qlangtech.tis.pubhook.common.RunEnvironment;
 import com.qlangtech.tis.rpc.server.IncrStatusUmbilicalProtocolImpl;
-import com.qlangtech.tis.solrj.extend.AbstractTisCloudSolrClient;
 import com.qlangtech.tis.sql.parser.TabPartitions;
 import com.qlangtech.tis.sql.parser.tuple.creator.EntityName;
 import com.qlangtech.tis.test.TISTestCase;
@@ -66,7 +65,7 @@ public class TestIndexSwapTaskflowLauncherWithSingleTableIndexBuild extends TIST
     String pt = "20200616170903";
 
     static {
-      //  AbstractTisCloudSolrClient.initHashcodeRouter();
+        //  AbstractTisCloudSolrClient.initHashcodeRouter();
     }
 
     @Override
