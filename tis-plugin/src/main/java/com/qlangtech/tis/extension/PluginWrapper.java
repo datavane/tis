0f7fee2480db61c0fc979dc8715a2d2d2abfd81b
mozhenghua <baisui@qlangtech.com>
2022-01-04 13:14:47 +0800

    add some property for PluginWrapper in use of UpdateCenter process

--- tis-plugin/src/main/java/com/qlangtech/tis/extension/PluginWrapper.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/extension/PluginWrapper.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qlangtech.tis.extension;
 
@@ -198,6 +198,7 @@ public class PluginWrapper implements Comparable<PluginWrapper>, ModelObject {
         return Boolean.parseBoolean(v) ? YesNoMaybe.YES : YesNoMaybe.NO;
     }
 
+
     /**
      * Set the list of components that depend optionally on this plugin.
      *
@@ -378,6 +379,23 @@ public class PluginWrapper implements Comparable<PluginWrapper>, ModelObject {
         return shortName;
     }
 
+    public String getGroupId() {
+        return manifest.getMainAttributes().getValue("Group-Id");
+    }
+
+    public long getLastModfiyTime() {
+        return Long.parseLong(manifest.getMainAttributes().getValue("Last-Modify-Time"));
+    }
+
+//    public long getArchiveFileSize() {
+//        //  this.archive.getAbsolutePath()
+//        return FileUtils.sizeOf(this.archive);
+//    }
+
+    public File getArchive() {
+        return this.archive;
+    }
+
     /**
      * Does this plugin supports dynamic loading?
      */
@@ -550,7 +568,7 @@ public class PluginWrapper implements Comparable<PluginWrapper>, ModelObject {
         }
         // add the optional dependencies that exists
         for (Dependency d : optionalDependencies) {
-            if (parent.getPlugin(d.shortName) != null){
+            if (parent.getPlugin(d.shortName) != null) {
                 dependencies.add(d);
             }
         }
