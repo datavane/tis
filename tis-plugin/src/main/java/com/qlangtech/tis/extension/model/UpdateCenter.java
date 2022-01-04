c73130de5f6ea11bc3dd8025ad5d72e0e0bf3184
mozhenghua <baisui@qlangtech.com>
2022-01-04 17:41:05 +0800

    refactor extract KEY_UPDATE_SITE and KEY_DEFAULT_JSON two params for UpdateCenter

--- tis-plugin/src/main/java/com/qlangtech/tis/extension/model/UpdateCenter.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/extension/model/UpdateCenter.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.qlangtech.tis.extension.model;
@@ -62,7 +62,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  **/
 public class UpdateCenter implements Saveable {
     public static final String PREDEFINED_UPDATE_SITE_ID = "default";
-    private static final String UPDATE_CENTER_URL;
+    public static final String KEY_UPDATE_SITE = "/update-site";
+    public static final String KEY_DEFAULT_JSON = "default.json";
+    private static final String UPDATE_CENTER_URL
+            = "http://mirror.qlangtech.com/" + TisMetaProps.getInstance().getVersion() + KEY_UPDATE_SITE + "/";
     public static final String ID_UPLOAD = "_upload";
     /**
      * An {@link ExecutorService} for updating UpdateSites.
@@ -122,7 +125,7 @@ public class UpdateCenter implements Saveable {
 //            LOGGER.info("Using a custom update center defined by the system property: {}", ucOverride);
 //            UPDATE_CENTER_URL = ucOverride;
 //        } else {
-        UPDATE_CENTER_URL = "http://mirror.qlangtech.com/" + TisMetaProps.getInstance().getVersion() + "/update-site/";
+        // UPDATE_CENTER_URL = ;
         //}
     }
 
@@ -338,7 +341,7 @@ public class UpdateCenter implements Saveable {
     }
 
     protected UpdateSite createDefaultUpdateSite() {
-        UpdateSite dftUpdateSite = new UpdateSite(PREDEFINED_UPDATE_SITE_ID, config.getUpdateCenterUrl() + "default.json");
+        UpdateSite dftUpdateSite = new UpdateSite(PREDEFINED_UPDATE_SITE_ID, config.getUpdateCenterUrl() + KEY_DEFAULT_JSON);
 //        if (!dftUpdateSite.existLocal()) {
 //
 //        }
