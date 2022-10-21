85c2f3cb94dae78481230d546a1a6377c53e6083
mozhenghua <baisui@qlangtech.com>
2022-10-21 15:21:05 +0800

    server version to 3.6.0-alpha

--- setversion.sh
+++ setversion.sh
@@ -1,1 +1,1 @@
-mvn versions:set -DnewVersion=3.6.0  -DprocessDependencies=true -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=3.6.0-alpha  -DprocessDependencies=true -DgenerateBackupPoms=false
