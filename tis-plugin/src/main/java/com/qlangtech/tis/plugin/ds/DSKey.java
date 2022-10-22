b505ab6177b9d93f7b3f90878512ece05e8652da
mozhenghua <baisui@qlangtech.com>
2022-10-22 10:35:58 +0800

    fix Nullpoint Exception for dbName

--- tis-plugin/src/main/java/com/qlangtech/tis/plugin/ds/DSKey.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/plugin/ds/DSKey.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qlangtech.tis.plugin.ds;
 
@@ -28,6 +28,10 @@ import java.util.Objects;
  */
 public class DSKey extends KeyedPluginStore.Key<DataSourceFactory> {
     private final DbScope dbScope;
+
+    public DSKey(String groupName, PostedDSProp dsProp, Class<DataSourceFactory> pluginClass) {
+        this(groupName, dsProp.getDbType(), dsProp.getDbname().get(), pluginClass);
+    }
 
     public DSKey(String groupName, DbScope dbScope, String keyVal, Class<DataSourceFactory> pluginClass) {
         super(groupName, keyVal, pluginClass);
--- tis-plugin/src/main/java/com/qlangtech/tis/plugin/ds/PostedDSProp.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/plugin/ds/PostedDSProp.java
@@ -1,26 +1,26 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qlangtech.tis.plugin.ds;
 
 import com.qlangtech.tis.offline.DbScope;
 import com.qlangtech.tis.util.UploadPluginMeta;
 
-import java.util.Objects;
+import java.util.Optional;
 
 /**
  * DS update extra params posted form client
@@ -34,7 +34,7 @@ public class PostedDSProp {
     public static final String KEY_TYPE = "type";
     public static final String KEY_UPDATE = "update";
 
-    private String dbname;
+    private Optional<String> dbname;
     private final DbScope dbType;
     private final Boolean update;
 
@@ -45,7 +45,7 @@ public class PostedDSProp {
     }
 
     public void setDbname(String dbname) {
-        this.dbname = dbname;
+        this.dbname = Optional.ofNullable(dbname);
     }
 
     public PostedDSProp(String dbname) {
@@ -58,17 +58,17 @@ public class PostedDSProp {
     }
 
     private PostedDSProp(String dbname, DbScope dbType, Boolean update) {
-        this.dbname = Objects.requireNonNull(dbname, "param dbName can not be null");
+        this.dbname = Optional.ofNullable(dbname); //Objects.requireNonNull(dbname, "param dbName can not be null");
         this.dbType = dbType;
         this.update = update;
     }
 
-    public String getDbname() {
-        return dbname;
+    public Optional<String> getDbname() {
+        return this.dbname;
     }
 
     public DbScope getDbType() {
-        return dbType;
+        return this.dbType;
     }
 
     public boolean isUpdate() {
