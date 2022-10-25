41bbed02be9bf595d92309599fd4f0d19a2c2377
mozhenghua <baisui@qlangtech.com>
2022-10-25 10:28:18 +0800

    skip none exist tpi file

--- tis-plugin/src/main/java/com/qlangtech/tis/extension/PluginManager.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/extension/PluginManager.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qlangtech.tis.extension;
 
@@ -146,6 +146,10 @@ public class PluginManager {
         // try (ACLContext context = ACL.as2(ACL.SYSTEM2)) {
         LOGGER.info("Attempting to dynamic load {}", arc);
         PluginManifest manifest = PluginManifest.create(arc);
+        if (manifest == null) {
+            LOGGER.warn("can not find tpi:" + arc.getAbsolutePath());
+            return;
+        }
 //        PluginWrapper p = null;
 //        String sn;
 //        try {
