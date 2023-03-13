41387a866e5ee46a2df4bb6bb99c836e8050b3bd
mozhenghua <baisui@qlangtech.com>
2023-03-13 14:34:11 +0800

    disable permitInitialize TIS when create cfgSnapshot from applied incr channel request

--- tis-plugin/src/main/java/com/qlangtech/tis/util/PluginMeta.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/util/PluginMeta.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.qlangtech.tis.util;
@@ -162,7 +162,10 @@ public class PluginMeta {
                 if (StringUtils.isNotEmpty(c = match.group(6))) {
                     classifier = Optional.of(PluginClassifier.create(c));
                 } else {
-                    Map<String, PluginWrapper> plugins = TIS.get().getPluginManager().getActivePluginsMap();
+                    TIS tis = TIS.get();
+                    // 可能TIS还没有初始化
+                    Map<String, PluginWrapper> plugins
+                            = (tis != null) ? tis.getPluginManager().getActivePluginsMap() : Collections.emptyMap();
                     if ((pw = plugins.get(pluginName)) != null) {
                         classifier = pw.getClassifier();
                     }
