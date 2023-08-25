8211e5b4dd23d3f6ab08c9207a9db847997cf49d
mozhenghua <baisui@qlangtech.com>
2023-08-25 17:30:18 +0800

    add hive reader connector support ,final statable

--- tis-plugin/src/main/java/com/qlangtech/tis/util/DescribableJSON.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/util/DescribableJSON.java
@@ -1,19 +1,19 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qlangtech.tis.util;
 
@@ -42,13 +42,11 @@ public class DescribableJSON<T extends Describable<T>> {
 
     public DescribableJSON(T instance, Descriptor<T> descriptor) {
         this.instance = Objects.requireNonNull(instance, "param instance can not be null");
-        this.descriptor = Objects.requireNonNull(descriptor
-                , "param descriptor can not be null,plugin type:" + instance.getClass());
+        this.descriptor = Objects.requireNonNull(descriptor, "param descriptor can not be null,plugin type:" + instance.getClass());
     }
 
     public DescribableJSON(T instance) {
-        this(Objects.requireNonNull(instance, "param instance can not be null")
-                , instance.getDescriptor());
+        this(Objects.requireNonNull(instance, "param instance can not be null"), instance.getDescriptor());
     }
 
     public JSONObject getItemJson() throws Exception {
@@ -77,7 +75,7 @@ public class DescribableJSON<T extends Describable<T>> {
 
 
         final JSON vals = pluginFormPropertyTypes.getInstancePropsJson(this.instance);
-        item.put("vals", vals);
+        item.put(AttrValMap.PLUGIN_EXTENSION_VALS, vals);
         if (instance instanceof IdentityName) {
             item.put("identityName", ((IdentityName) instance).identityValue());
         }
