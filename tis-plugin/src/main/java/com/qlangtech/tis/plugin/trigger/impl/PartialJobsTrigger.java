0fa8789d7e3271bcbb39c80f7433a4d22fdf3c49
mozhenghua <baisui@qlangtech.com>
2024-09-08 11:03:09 +0800

    change to  interface ManipuldateProcessor

--- tis-plugin/src/main/java/com/qlangtech/tis/plugin/trigger/impl/PartialJobsTrigger.java
+++ tis-plugin/src/main/java/com/qlangtech/tis/plugin/trigger/impl/PartialJobsTrigger.java
@@ -20,14 +20,11 @@ package com.qlangtech.tis.plugin.trigger.impl;
 
 import com.alibaba.citrus.turbine.Context;
 import com.google.common.collect.Lists;
-import com.qlangtech.tis.datax.IDataxProcessor;
 import com.qlangtech.tis.datax.impl.DataXCfgGenerator.GenerateCfgs;
-import com.qlangtech.tis.datax.impl.DataxProcessor;
 import com.qlangtech.tis.extension.Descriptor;
 import com.qlangtech.tis.extension.TISExtension;
-import com.qlangtech.tis.manage.common.AppAndRuntime;
 import com.qlangtech.tis.manage.common.Option;
-import com.qlangtech.tis.plugin.IPluginStore.AfterPluginSaved;
+import com.qlangtech.tis.plugin.IPluginStore.ManipuldateProcessor;
 import com.qlangtech.tis.plugin.IdentityName;
 import com.qlangtech.tis.plugin.annotation.FormField;
 import com.qlangtech.tis.plugin.annotation.FormFieldType;
@@ -35,15 +32,13 @@ import com.qlangtech.tis.plugin.annotation.Validator;
 import com.qlangtech.tis.plugin.trigger.JobTrigger;
 import com.qlangtech.tis.util.IPluginContext;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * 部份表同步任务触发执行器
  */
-public class PartialJobsTrigger extends JobTrigger implements AfterPluginSaved {
+public class PartialJobsTrigger extends JobTrigger implements ManipuldateProcessor {
 
     @FormField(ordinal = 1, type = FormFieldType.MULTI_SELECTABLE, validate = {Validator.require})
     public List<IdentityName> tabs = Lists.newArrayList();
@@ -55,27 +50,11 @@ public class PartialJobsTrigger extends JobTrigger implements AfterPluginSaved {
 
     public static List<Option> getTabsCandidate() {
         return GenerateCfgs.getTabsCandidate();
-//        AppAndRuntime appAndRuntime = AppAndRuntime.getAppAndRuntime();
-//        if (appAndRuntime == null) {
-//            return Collections.emptyList();
-//        }
-//
-//        IDataxProcessor dataxProcessor = DataxProcessor.load(null, appAndRuntime.getAppName());
-//        GenerateCfgs dataxCfgFileNames = dataxProcessor.getDataxCfgFileNames(null, Optional.empty());
-//
-//        return (dataxCfgFileNames.getTargetTabs().stream().map((tab) -> new Option(tab)).collect(Collectors.toList()));
-
-
     }
 
     @Override
-    public void afterSaved(IPluginContext pluginContext, Optional<Context> context) {
-//        if (!context.isPresent()) {
-//            throw new IllegalStateException("context must be present");
-//        }
+    public void manipuldateProcess(IPluginContext pluginContext, Optional<Context> context) {
         this.setPartialTrigger2Context(context, this);
-//        context.get().put(JobTrigger.class.getName(), this);
-        //  System.out.printf(this.tabs.stream().map((tab) -> tab.identityValue()).collect(Collectors.joining(",")));
     }
 
     @TISExtension
