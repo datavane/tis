c0bd4833a7e994e93c376fedee580eeafc12b690
mozhenghua <baisui@qlangtech.com>
2024-09-27 22:34:25 +0800

    set tis-console contenxt classLoader in Spring AppicationContenxt initialize

--- tis-console/src/main/java/com/qlangtech/tis/manage/spring/TISWebApplicationContext.java
+++ tis-console/src/main/java/com/qlangtech/tis/manage/spring/TISWebApplicationContext.java
@@ -1,25 +1,26 @@
 /**
- *   Licensed to the Apache Software Foundation (ASF) under one
- *   or more contributor license agreements.  See the NOTICE file
- *   distributed with this work for additional information
- *   regarding copyright ownership.  The ASF licenses this file
- *   to you under the Apache License, Version 2.0 (the
- *   "License"); you may not use this file except in compliance
- *   with the License.  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.qlangtech.tis.manage.spring;
 
 import com.qlangtech.tis.manage.common.Config;
 import com.qlangtech.tis.web.start.TisAppLaunch;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -41,6 +42,13 @@ public class TISWebApplicationContext extends XmlWebApplicationContext {
   @Override
   protected ResourcePatternResolver getResourcePatternResolver() {
     return new TISResourcePatternResolver(this);
+  }
+
+  @Override
+  protected DefaultListableBeanFactory createBeanFactory() {
+    DefaultListableBeanFactory beanFactory = super.createBeanFactory();
+    beanFactory.setBeanClassLoader(TISWebApplicationContext.class.getClassLoader());
+    return beanFactory;
   }
 
   private static class TISResourcePatternResolver extends ServletContextResourcePatternResolver {
