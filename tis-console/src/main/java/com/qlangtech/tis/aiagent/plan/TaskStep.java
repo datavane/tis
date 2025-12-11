60a3673433030a39e230a7944a22adbce64937fb
baisui1981 <baisui@qlangtech.com>
2025-12-11 14:09:08 +0800

    reuse PLUGIN_CREATE(new PluginInstanceCreateExecutor())

--- tis-console/src/main/java/com/qlangtech/tis/aiagent/plan/TaskStep.java
+++ tis-console/src/main/java/com/qlangtech/tis/aiagent/plan/TaskStep.java
@@ -50,7 +50,7 @@ public class TaskStep {
 
   public enum StepType {
     PLUGIN_INSTALL(new PluginDownloadAndInstallExecutor()),
-    //PLUGIN_CREATE(new PluginInstanceCreateExecutor4Mock()),
+   // PLUGIN_CREATE(new PluginInstanceCreateExecutor4Mock()),
     PLUGIN_CREATE(new PluginInstanceCreateExecutor()),
     USER_INPUT(null),
     EXECUTE_BATCH(new PipelineBatchExecutor()),
