032c0b08df0c7965fdafd996810d2006dbdd0949
baisui1981 <baisui@qlangtech.com>
2026-07-19 16:13:19 +0800

    move spring dependency to tis-web-start module ,as class in package 'org.springframework.orm.ibatis' has been moved to tis-ibatis artifact that will be loaded in tis root classloader environment

--- tis-web-start/src/main/java/com/qlangtech/tis/web/start/TisApp.java
+++ tis-web-start/src/main/java/com/qlangtech/tis/web/start/TisApp.java
@@ -32,6 +32,7 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.ServiceLoader;
 
 /**
@@ -120,7 +121,7 @@ public class TisApp {
         File root = getWebRootDir();
         getLogger().info("webapps context dir:{}", root.getAbsolutePath());
         File contextDir = null;
-        for (String context : root.list()) {
+        for (String context : Objects.requireNonNull(root.list())) {
             contextDir = new File(root, context);
             if (contextDir.isDirectory() && !TisSubModule.WEB_START.moduleName.equals(context)) {
                 if (APP_CONSOLE.equals(context) //|| TisSubModule.ZEPPELIN.moduleName.equals(context)
